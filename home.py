827c3bb841f7e213839ae906fa8279670bbbe94e
DeivisonSouza <souzaflorestal@gmail.com>
2024-07-23 11:56:22 -0300

    Update

--- home.py
+++ home.py
@@ -56,7 +56,7 @@ st.write("# Welcome! 👋")
 #st.title(""" 👉 SOBRE A APLICAÇÃO WEB """)
 st.markdown("""🌿𓂃⋅𓂃⋅𓂃⋅𓂃⋅𓂃⋅𓂃⋅𓂃⋅𓂃⋅𓂃⋅🌿""")
 st.markdown("<h3 style='text-align: left; color: darkgreen;'>🌳DEEP BARK PROJECT       𓂃⋅𓂃⋅𓂃⋅𓂃⋅𓂃⋅𓂃⋅𓂃⋅</h3>", unsafe_allow_html=True)
-st.write("######")
+#st.write("######")
 
 # st.markdown(
 # """
